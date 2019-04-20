889354f4882695c9143248f13913705474a368c4
George Rennie <georgerennie@gmail.com>
2019-04-20 23:24:53 +0100

    added comments to SerialRead

--- Firmware/src/SerialRead.c
+++ Firmware/src/SerialRead.c
@@ -6,7 +6,7 @@ uint8_t mRxBufferHead = 0;
 uint8_t mRxBufferTail = 0;
 uint8_t mDataInBuffer = 0;
 
-//Reads from ring buffer
+//Reads a byte from the ring buffer
 int serialReadByte(void) {
   if (!mDataInBuffer) {
     return -1;
@@ -19,10 +19,12 @@ int serialReadByte(void) {
 }
 
 uint8_t serialAvailable(void) {
-  //returns 0 only when the buffer pointers are the same value
-  //i.e. whenever there is data in the buffer the pointers are at dif places
-  //Therefore it returns the number of bytes in buffer
-  return (uint8_t)(RX_BUFFER_SIZE + mRxBufferHead - mRxBufferTail) % RX_BUFFER_SIZE;
+  /*Returns 0 only when the buffer pointers are the same value
+    i.e. whenever there is data in the buffer the pointers are at dif places
+    therefore it returns the number of bytes in buffer
+  */
+  return (uint8_t)(RX_BUFFER_SIZE + mRxBufferHead - mRxBufferTail)
+    % RX_BUFFER_SIZE;
 }
 
 void serialSetup(void) {
@@ -36,14 +38,14 @@ void serialSetup(void) {
   UCSR0A &= ~(1 << U2X0);
   #endif
 
-  UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); //use 8 bit serial
+  UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); //Use 8 bit serial
   UCSR0C &= ~((1 << UMSEL00) |(1 << UMSEL01)); //Use Asynch USART
   UCSR0C &= ~((1 << UPM00) |(1 << UPM01)); //Use no parity
   UCSR0C &= ~(1 << USBS0); //Use 1 Stop Bit
-  UCSR0B |= (1 << RXEN0); //enable serial rx
+  UCSR0B |= (1 << RXEN0); //Enable serial rx
 
   UCSR0B |= (1 << RXCIE0); //Enable rx intterupt
-  sei(); //allow global interrupts
+  sei(); //Allow global interrupts
 }
 
 //Called on new data in rx buffer interrupt
@@ -51,7 +53,9 @@ static void rxComplete(void) {
   //Standard cyclic buffer
   uint8_t data = UDR0;
 
-  //Dont let the head (i.e. position of latest data) catch the tail (i.e pos of earliest unread data)
+  /*Dont let the head (i.e. position of latest data)
+    catch the tail (i.e pos of earliest unread data)
+  */
   if ((mRxBufferHead != mRxBufferTail) || !mDataInBuffer) {
     mRxBuffer[mRxBufferHead] = data;
     mRxBufferHead = (mRxBufferHead + 1) % RX_BUFFER_SIZE;
--- Firmware/src/SerialRead.h
+++ Firmware/src/SerialRead.h
@@ -6,7 +6,16 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/*Read a single byte from the serial buffer if available.
+  Returns -1 if there is no byte in the buffer
+*/
 int serialReadByte(void);
+
+/*Returns non zero if there is at least one byte in the
+  serial buffer to be read
+*/
 uint8_t serialAvailable(void);
+
+//Intialises registers for serial reading at baud rate defined by BAUD_RATE 
 void serialSetup(void);
 #endif
