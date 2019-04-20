f037dfb9e3caf1cb9fa5774bff584b5fd17ba034
George Rennie <georgerennie@gmail.com>
2019-04-20 23:26:13 +0100

    cleaned up comments in Settings.h

--- Firmware/src/Settings.h
+++ Firmware/src/Settings.h
@@ -5,14 +5,16 @@
 #define SETTINGS_H
   #include <inttypes.h>
 
-  //This code makes vscode play ball with libc but doesnt affect production
-  //Set the define to the correct processor as in avr/io.h
+  /*This code makes vscode play ball with libc but doesnt affect production
+    Set the define to the correct processor as in avr/io.h
+  */
   #if !defined (__AVR__)
     #define __AVR_ATmega328P__
   #endif
   
   #include <avr/io.h>
-  #include "ioPinsSettings.h" //This defines all the iopins and iopin structs, use it to define io pins
+  //This defines all the iopins and iopin structs, use it to define io pins
+  #include "ioPinsSettings.h"
 
   //CPU Freq
   #define F_CPU 16000000
@@ -22,13 +24,16 @@
   //Treat note on messages with 0 velocity as note off messages (advised)
   #define NULL_VELOCITY_AS_NOTE_OFF 1
 
-  //Serial rx buffer size in bytes (works best as powers of 2) cant be more than 8 bit
+  /*Serial rx buffer size in bytes (works best as powers of 2).
+    This cant be more than 8 bit
+  */
   #define RX_BUFFER_SIZE 32
 
   //Number of channels (starting from channel 0) that polytomono and main use
   #define MIDI_CHANNELS 2
 
-  static const uint8_t NOTE_RANGE[] = {48, 96}; //Define notes for different accuracies
+  //Define notes for different accuracies
+  static const uint8_t NOTE_RANGE[] = {48, 96};
   static const uint8_t LOWEST_NOTE[] = {24, 24};
   static const uint8_t HIGHEST_NOTE[] = {72, 120};
 #endif
