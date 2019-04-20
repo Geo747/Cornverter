7da52dcd2ed59a7215a218e95289dbb37aa1ec52
George Rennie <georgerennie@gmail.com>
2019-04-20 11:49:49 +0100

    Added function comments to DigitalOutputs.h

--- Firmware/src/DigitalOutputs.h
+++ Firmware/src/DigitalOutputs.h
@@ -4,8 +4,17 @@
 
 #include "Settings.h"
 
+//Write a state to the gate pin corresponding to that channel
 void digitalOutputsUpdateGate(uint8_t state, uint8_t channel);
+
+/*Write a state to a digital output pin.
+  The output is selected by output number and channel
+*/
 void digitalOutputsUpdateDigi(uint8_t state, uint8_t channel, uint8_t output);
+
+//Write a state to the clock output pin
 void digitalOutputsUpdateClock(uint8_t state);
+
+//Write a state to the reset output pin
 void digitalOutputsUpdateReset(uint8_t state);
 #endif
