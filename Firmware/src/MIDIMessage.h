96bd3272315d5692464d275d8c41126295cd8ce5
George Rennie <georgerennie@gmail.com>
2019-04-20 11:40:06 +0100

    tidied comments in MIDIMessage.h

--- Firmware/src/MIDIMessage.h
+++ Firmware/src/MIDIMessage.h
@@ -5,27 +5,26 @@
 #include "MIDIDefs.h"
 
 typedef struct {
-  /*! The MI
-  DI channel on which the message was recieved.
-    \n Value goes from 0 to 15.
+  /*The MIDI channel on which the message was recieved
+    Value goes from 0 to 15
     16 represenents an unknown channel
-    */
+  */
   uint8_t channel;
 
-  /*! The type of the message
+  /*The type of the message
     (see the MIDIType enum for types reference)
-    */
+  */
   MIDIType type;
 
-  /*! The first data byte.
-    \n Value goes from 0 to 127.
-    */
+  /*The first data byte
+    Value goes from 0 to 127
+  */
   uint8_t data1;
 
-  /*! The second data byte.
-    If the message is only 2 bytes long, this one is null.
-    \n Value goes from 0 to 127.
-    */
+  /*The second data byte
+    If the message is only 2 bytes long, this one is null
+    Value goes from 0 to 127
+  */
   uint8_t data2;  
 } MIDIMessage;
 #endif
